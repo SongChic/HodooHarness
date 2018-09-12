283ab5d57490db0f286665bc2030d4451bb47b85
SongChic <songchic2@gmail.com>
2018-09-12 10:40:02 +0900

    bash file second modify

--- git_just_push.sh
+++ git_just_push.sh
@@ -1,4 +1,16 @@
 #text="$1"
+if [ ! -n "$1" ]
+then
+	echo "comment를 입력해주세요."
+	exit
+elif [ ! -n "$2" ]
+then
+	echo "branch를 입력해주세요."
+	exit
+fi
+
+echo "git start"
+
 git add .
 git commit -m "$1"
-#echo $text
+git push origin $2
