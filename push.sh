00044affe18c781a0440b25ce7f35faa6382b8ac
SongChic <songchic2@gmail.com>
2018-09-12 11:35:30 +0900

    bash file modify fin

--- push.sh
+++ push.sh
@@ -1,16 +1,36 @@
+#!/bin/bash
 #text="$1"
+commit=""
+branch=master
 if [ ! -n "$1" ]
 then
-	echo "comment를 입력해주세요."
-	exit
-elif [ ! -n "$2" ]
+	read -n 1 -p "comment 없이 push 하시겠습니까? (Y/n) : "
+	echo -e "\n$REPLY"
+	if [ "$REPLY" != "Y" -a "$REPLY" != "y" ]
+	then
+		read -p "commit 내용을 입력해주세요 : "
+		commit=$REPLY
+		echo -e "\n$commit"
+	fi
+	#exit
+fi
+if [ ! -n "$2" ]
 then
-	echo "branch를 입력해주세요."
-	exit
+	read -n 1 -p "master branch를 사용하시겠습니까? (Y/n)"
+	if [ "$REPLY" == "Y" -o "$REPLY" == "y" ]
+	then
+		branch=master
+	else
+		echo -e "\n"
+		read -p "사용할 브랜치를 입력해주세요 : " temp_branch
+		echo -e "\n$temp_branch"
+		branch=$temp_branch
+	fi
+	#exit
 fi
 
 echo "git start"
 
 git add .
-git commit -m "$1"
-git push origin $2
+git commit -m "$commit"
+git push origin $branch
